e275474679e33822f11fe34979f91ba028b96ed9
이승주 <mz_shugo@woowafriends.com>
2023-06-13 16:06:28 +0900

    ˆadd: base components.

---
+++ src/components/base/Breadcrumb.svelte
@@ -0,0 +1,11 @@
+<script>
+	import { Breadcrumb, BreadcrumbItem } from 'flowbite-svelte';
+
+	export let pageList;
+</script>
+
+<Breadcrumb aria-label='Default breadcrumb example'>
+	{#each pageList as { link, name }}
+		<BreadcrumbItem href={link} home>{name}</BreadcrumbItem>
+	{/each}
+</Breadcrumb>
---
+++ src/components/base/ButtonGroup.svelte
@@ -0,0 +1,12 @@
+<script>
+	import { ButtonGroup, Button } from 'flowbite-svelte';
+// 	slot? list?
+
+	// export let ;
+</script>
+
+<ButtonGroup>
+	<Button>Profile</Button>
+	<Button>Settings</Button>
+	<Button>Messages</Button>
+</ButtonGroup>
